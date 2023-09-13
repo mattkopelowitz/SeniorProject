b30bd93370c43998cc2d35e86e8f37ae6943bf3d
Meryem Yuksel <myuksel@ufl.edu>
2023-09-13 13:55:54 -0400

    made it so that it doesn't ignore the build folder

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+//Connecting to database in MongoDB -> we still need to set up mongoBD though
+mongoose.connect('mongodb://localhost/my-database', { useNewUrlParser: true });
+
+//makes it so that we can read the data held in JSON format in MongoDB
+app.use(bodyParser.json());
+
+//connecting to front-end through the same port
 const port = process.env.PORT || 3000;
 
 // Define outes and middleware here
