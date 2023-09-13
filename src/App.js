f1adfbf6858d31c67a9dccf4c5a35680ae176542
Meryem Yuksel <myuksel@ufl.edu>
2023-09-13 14:48:40 -0400

    added three category options with some style that might be modified later

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  //handles when you select a category as true or false
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+
+  //categories to specify the search with 
+  const categories = [
+    'Price Range',
+    'Open Hours',
+    'Business Type',
+  ];
+
+  //function that shows a pop-up when you select a category
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    setShowPopup(true);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +30,23 @@ function App() {
           <button>Search</button>
         </div>
       </header>
+      <div className="categories">
+        <h2>Categories</h2>
+        <ul>
+          {categories.map((category, index) => (
+            <li key={index}>
+               <button onClick={() => handleCategoryClick(category)}>{category}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {showPopup && (
+        <div className="popup">
+          <h3>{selectedCategory} Options</h3>
+          {/* Add options and selection logic for the selected category */}
+          <button onClick={() => setShowPopup(false)}>Close</button>
+        </div>
+      )}
     </div>
   );
 }
