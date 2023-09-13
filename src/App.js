386330a088180b1c6ca11c6e0ad05dae6c0526a6
Meryem Yuksel <myuksel@ufl.edu>
2023-09-13 14:34:31 -0400

    added some styling and a static search bar for the front page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css'; // You can style your components in App.css
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>GatorAdvisor</h1>
+      <header className="App-header">
+        <h1>GatorAdvisor</h1>
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+          <button>Search</button>
+        </div>
+      </header>
     </div>
   );
 }
