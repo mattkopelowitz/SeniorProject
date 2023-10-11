19e9142651648501ea20bfe2e73bb88793ad87c3
Meryem Yuksel <myuksel@ufl.edu>
2023-10-11 12:34:23 -0400

    organized to frontend and backend folders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './frontend/index.css';
+import App from './frontend/App';
 import reportWebVitals from './reportWebVitals';
 
 <script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
